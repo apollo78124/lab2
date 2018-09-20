b803adc62783e6e8c6bec75543f91832f2dabb8c
apollo78124 <98apollo@naver.com>
2018-09-20 14:38:56 -0700

    saf

--- main.cpp
+++ main.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
+#include<fstream>
+#include <random>
+#include <ctime>
+using namespace std;
+
+int writeFile() {
+    int random = 0;
+    ofstream myfile;
+    myfile.open ("Readings.txt");
+
+    random_device rd;
+    mt19937 generator(rd());
+    uniform_int_distribution<> distribution(512, 1024);
+    random = distribution(generator);
+
+    default_random_engine generator1(time(0));
+    uniform_real_distribution<double> distribution1(50.000, 90.000);
+
+    for (int i = 0; i < random; i++) {
+        myfile << i << " ";
+        myfile << distribution1(generator1);
+        myfile <<"\n";
+    }
+    myfile.close();
+    return 1;
+}
+
+int calculation() {
+    return 1;
+}
 
 int main() {
-    using namespace std;
+
+    writeFile();
+
     cout << "Hello, World!" << endl;
 
     return 0;
